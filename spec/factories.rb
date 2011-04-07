0e0256588e17d3c66ec8119b985a07d93aa50787
Clark Russell <clarkarussell@gmail.com>
2011-04-07 10:09:08 -0700

    Added a User factory and tests for show page

---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+Factory.define :user do |user|
+	user.name										"Clark Russell"
+	user.email									"clark.russell@bfa.org"
+	user.password								"foobar"
+	user.password_confirmation  "foobar"
+end
+
