574065b41bb824264fc03594e9299280c94f7589
Clark Russell <clarkarussell@gmail.com>
2011-04-19 11:34:55 -0700

    Handling invalid signin

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -16,4 +16,29 @@ render_views
 		end
   end
 
+	describe "POST 'create'" do
+	
+		describe "failure" do
+	
+			before(:each) do
+				@attr = { :email => "", :password => ""}
+			end
+		
+			it "should re-render the new page" do
+				post :create, :session => @attr
+			end
+
+			it "should have an error message" do
+				post :create, :session => @attr
+				flash.now[:error].should =~ /invalid/i
+			end
+
+			it "should have the right title" do
+				post :create, :session => @attr
+				response.should have_selector('title', :content => "Sign in")
+			end
+
+		end
+	end
+
 end
