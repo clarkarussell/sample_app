902e1127b77967233c8a3fe2819c447d0de4f677
Clark Russell <clarkarussell@gmail.com>
2011-05-12 10:31:55 -0700

    Microposts validations

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -25,7 +25,22 @@ describe Micropost do
 		it "should have the right associated user" do
 			@micropost.user_id.should == @user.id
 			@micropost.user.should == @user
-		end	
-	
+		end		
 	end
+
+	describe "validations" do
+	
+		it "should have a user id" do
+			Micropost.new(@attr).should_not be_valid
+		end
+	
+		it "should require nonblank content" do
+			@user.microposts.build(:content => "        ").should_not be_valid
+		end
+	
+		it "should reject long content" do
+			@user.microposts.build(:content => "a" * 141).should_not be_valid 
+		end
+	end
+
 end
